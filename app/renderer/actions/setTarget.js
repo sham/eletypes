f86618b5d69556206037f00c0dd14eaab055c649
しゃむ <shamofu@gmail.com>
2018-01-10 12:06:57 +0900

    Add SET_TARGET action

---
+++ app/renderer/actions/setTarget.js
@@ -0,0 +1,2 @@
+export const SET_TARGET = '@@eletypes/SET_TARGET';
+export const setTarget = (keyCode) => ({ type: SET_TARGET, payload: keyCode });
--- app/renderer/reducers/reducer.js
+++ app/renderer/reducers/reducer.js
@@ -1,6 +1,8 @@
 import { KEY_TOGGLE } from '../actions/keyToggle';
+import { SET_TARGET } from '../actions/setTarget';
 
 const initialState = {
+  target: null,
   keyState: {}
 };
 
@@ -8,6 +10,9 @@ export default function reducer(state = initialState, action) {
   switch (action.type) {
     case KEY_TOGGLE: {
       return { ...state, keyState: { ...state.keyState, [action.payload]: !state.keyState[action.payload] } };
+    }
+    case SET_TARGET: {
+      return { ...state, target: action.payload };
     }
     default: {
       return state;
